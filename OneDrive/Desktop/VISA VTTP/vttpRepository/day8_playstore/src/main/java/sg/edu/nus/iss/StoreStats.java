5095ccdbb522248a44fdcdadc69f8aefa4b6a165
russell258 <duckie258@gmail.com>
2023-05-05 23:28:59 +0800

    store stat class

---
+++ OneDrive/Desktop/VISA VTTP/vttpRepository/day8_playstore/src/main/java/sg/edu/nus/iss/StoreStats.java
@@ -0,0 +1,17 @@
+package sg.edu.nus.iss;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StoreStats {
+    private String category;
+    private float highestRating = -1;
+    private String highestRatedApp;
+    private float lowestRating = 6;
+    private String lowestRatedApp;
+    private List<Float> allRatings = new LinkedList<>();
+
+
+    
+}
